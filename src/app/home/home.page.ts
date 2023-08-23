f2c2819f5388a2570a3124c07cb83f98f1c07d75
Tamilyn <128110443+babimeaw@users.noreply.github.com>
2023-08-23 16:48:28 -0300

    Add files via upload

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Contato } from 'src/app/model/entities/contato';
+
+@Component({
+selector: 'app-home',
+templateUrl: 'home.page.html',
+styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+nome!: string;
+telefone! : number;
+email ! : string;
+lista_contatos: Contato[] = [];
+
+constructor(private alertController: AlertController) {
+let c1 : Contato = new Contato("Tamilyn Camille Costa",91094415, "tamilyn.cc@hotmail.com.br")
+let c2 : Contato = new Contato("Yasmim Haffermann Bilovus",91094400, "yasmimhb@gmail.com")
+let c3 : Contato = new Contato("Ermindo Treviso Neto",91091115,"ermindone@gmail.com" )
+let c4 : Contato = new Contato("Gabriel Dibs",91091100, "dibzinho@gmail.com")
+this.lista_contatos.push(c1);
+this.lista_contatos.push(c2);
+this.lista_contatos.push(c3);
+this.lista_contatos.push(c4);
+
+}
+
+cadastrar(){
+if(!this.nome || !this.telefone || this.email){
+this.presentAlert("Erro", "Todos os campos são obrigatórios!");
+}else{
+this.presentAlert("Sucesso", "Contato Cadastrado!");
+}
+let novo : Contato = new Contato(this.nome, this.telefone, this.email);
+this.lista_contatos.push(novo);
+}
+
+async presentAlert(subHeader: string, message: string) {
+const alert = await this.alertController.create({
+header: 'Agenda de Contatos',
+subHeader: subHeader,
+message: message,
+buttons: ['OK'],
+});
+
+await alert.present();
+}
+}
---
+++ src/app/model/entities/contato.ts
@@ -0,0 +1,31 @@
+export class Contato{
+    private _nome: string;
+    private _telefone: number;
+    private _email : string;
+    
+    public get nome(): string {
+        return this._nome;
+    }
+    public set nome(value: string) {
+        this._nome = value;
+    }
+
+    public get telefone(): number {
+        return this._telefone;
+    }
+    public set telefone(value: number) {
+        this._telefone = value;
+    }
+    public get email(): string {
+        return this._email;
+    }
+    public set email(value: string) {
+        this._email = value;
+    }
+
+    constructor(nome:string, telefone: number, email: string){
+        this._nome = nome;
+        this._telefone = telefone;
+        this._email = email;
+    }
+}
